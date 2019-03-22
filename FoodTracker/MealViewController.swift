4ed52c1c77632006ee625c035e8cb95643787e1c
yunyou <1205361247@qq.com>
2019-03-22 13:43:00 +0800

    feat: 1. 将tableViewController作为项目入口，并通过IB在table中绘制tableCell原型 2. 继承UITableViewCell实现MealTableViewCell类，并通过@IBOutlet建立关联 3. 继承UITableViewController实现MealTableViewController类，在该类中初始化列表数据，重写相应的方法来填充表格，通过id来连接MealTableViewCell

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by 文达黄 on 2019/3/19.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
+//    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -37,7 +37,7 @@ class ViewController: UIViewController, UITextFieldDelegate,UIImagePickerControl
         return true
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
+//        mealNameLabel.text = textField.text
     }
     
     //MARK: Actions
