680f375f3e8e0eca0dee472fe0a92a2bbde2fbb3
yunyou <1205361247@qq.com>
2019-03-22 15:30:22 +0800

    feat: 1. 将MealTableViewController包括在navigation controller中，给导航栏改名字，添加“+”按钮 2. 通过“+”按钮建立show segue到MealViewController 3. 将MealViewController包括在navigation controller中，给导航栏改名字，添加“cancel”按钮、“save”按钮 4. 在MealViewController中添加saveButton、meal、prepare函数。在prepare函数中存储新增的meal信息 5. 在MealTableViewController中添加@IBAction的unwind函数，用于将获取到的meal展示在列表中 6. 在IB中将save指向exit建立unwind关联，关联函数为MealTableViewController中的unwind函数 7. saveButton被点击后，会先调用source view的prepare函数，然后调用des view指定的unwind函数 8. 在MealViewController中监听键盘输入，输入时save按钮不可用，且字段空时也不可用 9. 在IB中建立cancel按钮到MealViewController的关联@IBAction，用于取消操作

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -15,6 +16,13 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
 //    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    /*
+     This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+     or constructed as part of adding a new meal.
+     */
+    var meal: Meal?
     
     
     override func viewDidLoad() {
@@ -23,6 +31,9 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
         
         // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
+        
+        // Enable the Save button only if the text field has a valid Meal name.
+        updateSaveButtonState()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,8 +47,36 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
         textField.resignFirstResponder()
         return true
     }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.isEnabled = false
+    }
     func textFieldDidEndEditing(_ textField: UITextField) {
-//        mealNameLabel.text = textField.text
+        updateSaveButtonState()
+        navigationItem.title = textField.text
+    }
+    
+    //MARK: Navigation
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+         dismiss(animated: true, completion: nil)
+    }
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        
+        // Set the meal to be passed to MealTableViewController after the unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
     }
     
     //MARK: Actions
@@ -72,6 +111,13 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
         
         // Dismiss the picker.
         dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Private Methods
+    private func updateSaveButtonState() {
+        // Disable the Save button if the text field is empty.
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
     }
 }
 
